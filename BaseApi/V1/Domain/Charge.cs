bda28f63d15c7543dbf65a778a138cf55dff2814
Hamid Jolany <hamidjolany@nudgedigital.co.uk>
2021-05-20 17:25:52 +0430

    Charge Entity Definition & Remove default Entity structure

---
+++ BaseApi/V1/Domain/Charge.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace BaseApi.V1.Domain
+{
+    //TODO: rename this class to be the domain object which this API will getting. e.g. Residents or Claimants
+    public class Charge
+    {
+        public int Id { get; set; }
+        public string ChargeType { get; set; }
+        public string ChargeSource { get; set; }
+        public string DebitCode { get; set; }
+        public string DebitCodeDescription { get; set; }
+        public DateTime EffectiveStartDate { get; set; }
+        public DateTime TerminationDate { get; set; }
+        public string PeriodCode { get; set; }
+        public DateTime DebitNextDue { get; set; }
+        public DateTime DebitLastCharged { get; set; }
+        public bool DebitActive { get; set; }
+        public decimal DebitValue { get; set; }
+        public bool PropertyDebit { get; set; }
+        public string DebitSource { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string ServiceChargeSchedule { get; set; }
+        public string DataImportSource { get; set; }
+
+        //TODO: Add fields which you are interested in
+    }
+}
